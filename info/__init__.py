4939a505531be3b6bf2b30e867f1b204c21b087f
lesuhcke6 <z1327252912692483@163.com>
2021-06-12 22:12:18 +0800

    数据库对象全局化

--- info/__init__.py
+++ info/__init__.py
@@ -5,17 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
 from config import config_dict
 
+# 将数据库链接对象定义为全局变量 以便其他文件导入使用
+db = None   # type: SQLAlchemy
+rs = None   # type: Redis
+
 # 工厂函数: 由 外界 提供物料,函数内部封装对象的创建过程
-
-
-
 def creat_app(config_type):  # 封装web应用创建过程
     # 根据类型取出对应的配置子类
     config_class = config_dict[config_type]
-
     app = Flask(__name__)
     # 数据库设置
     app.config.from_object(config_class)
+
+    # 声明全局变量
+    global db,rs
 
     # 创建数据库链接
     db = SQLAlchemy(app)
--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,7 +1,9 @@
 from info.modules.home import home_blu
+from info import rs
 
 # 使用蓝图对象注册路由
 @home_blu.route("/")
 def index():
+    rs.set("age",20)
 
     return "index"
